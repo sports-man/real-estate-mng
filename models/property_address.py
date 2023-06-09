e536d9786e2438b6a4a2d79e4061c1d51a6c9009
sports-man <sportsman0701@gmail.com>
2023-06-08 22:28:20 -0400

    [WIP] working in property address model

--- models/property_address.py
+++ models/property_address.py
@@ -6,7 +6,7 @@ class PropertyAddress(models.Model):
     _description = 'Property Address'
 
     street = fields.Char(string='Street', required=True)
-    street2 = fields.Char(string='Calle 2')
+    street2 = fields.Char(string='Street 2')
     city = fields.Char(string='Ciudad', required=True)
     state_id = fields.Many2one('res.country.state', string='Provincia', required=True)
     country_id = fields.Many2one('res.country', string='País', required=True)
