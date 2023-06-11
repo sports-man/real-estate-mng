7bc5f5a0cb0d13240a880db50ec671472ddfef3f
sports-man <sportsman0701@gmail.com>
2023-06-11 17:44:25 -0400

    [WIP] working in the property model

--- models/property.py
+++ models/property.py
@@ -10,16 +10,16 @@ class Property(models.Model):
     description = fields.Text(string='Description', required=True)
     address_id = fields.Many2one('property.address', string='Address', required=True)
     price = fields.Float(string='Price', required=True)
-    num_bedrooms = fields.Integer(string='Número de Habitaciones')
-    num_bathrooms = fields.Integer(string='Número de Baños')
-    total_area = fields.Char(string='Área Total de la Propiedad', required=True)
-    availability_date = fields.Date(string='Fecha de Disponibilidad', required=True)
+    num_bedrooms = fields.Integer(string='Number of bedrooms')
+    num_bathrooms = fields.Integer(string='Number of bathrooms')
+    total_area = fields.Char(string='Area total', required=True)
+    availability_date = fields.Date(string='Availability date', required=True)
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
-    image = fields.Binary(string='Imagen')
+    image = fields.Binary(string='Image')
 
     @api.constrains('price')
     def _check_price_positive(self):
     # Checks if the price of a record is positive and raises a validation error if it is not.
         for record in self:
             if record.price <= 0:
-                raise ValidationError("El precio debe ser mayor que cero.")
+                raise ValidationError("The price must be grater that zero.")
